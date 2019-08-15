df2b90a15ad8ea647f26b8319d722522b76974fc
Ethan Fulcher <fulchere@msu.edu>
2019-08-15 12:11:43 -0400

    updates, voltage now working

--- python3.5m/linux-x86_64/iseg.py
+++ python3.5m/linux-x86_64/iseg.py
@@ -19,28 +19,20 @@ class isegSupport(PT.TableBase):
     s = PT.Parameter(iointr=True)
     time = PT.Parameter(iointr=True)
     temp = PT.Parameter(iointr=True)
-    
+    ws = None
+    session_id = None
+    volt0 = PT.Parameter(iointr=True)
+    volt1 = PT.Parameter(iointr=True)
+    volt2 = PT.Parameter(iointr=True)
+    volt3 = PT.Parameter(iointr=True)
+    volt4 = PT.Parameter(iointr=True)
+    volt5 = PT.Parameter(iointr=True)
+    volt6 = PT.Parameter(iointr=True)
+    volt7 = PT.Parameter(iointr=True)
+
     def __init__(self, name):
-        super().__init__(name='iseg')    
-
-    def calls(self,ws):
-        for i in range(10):
-            received = ws.recv()
-            jsoned = json.loads(received)
-            for js in jsoned:            
-                if 'c' in js:
-                    for item in js['c']:
-                        typ = item['d']['i']
-                        if typ == "System.time":
-                            self.time.value = item['d']['v']
-                            self.time.notify()
-                        if typ == "Status.temperature0" or typ == "Status.temperature1":
-                            self.temp.value = item['d']['v']
-                            self.temp.notify()
-
-    @trigger.onproc
-    def do_trigger(self):
-        ws = create_connection("ws://192.168.1.101:8080",timeout=40)
+        super().__init__(name='iseg')
+        self.ws = create_connection("ws://192.168.1.101:8080",timeout=40)
         login_request = '''{
 	                    "i": "",
 	                    "t": "login",
@@ -51,9 +43,53 @@ class isegSupport(PT.TableBase):
 	                    },
 	                    "r": "websocket"
                         }'''
-        ws.send(login_request)
-        login_message = ws.recv()
-        session_id = json.loads(login_message)['i']
+        self.ws.send(login_request)
+        login_message = self.ws.recv()
+        self.session_id = json.loads(login_message)['i']
+
+    def thread_call(self):
+        for i in range(1):
+            received = self.ws.recv()
+            jsoned = json.loads(received)
+            for js in jsoned:            
+                if 'c' in js:
+                    for item in js['c']:
+                        typ = item['d']['i']
+                        if typ == "System.time":
+                            self.time.value = item['d']['v']
+                            self.time.notify()
+                        if typ == "Status.temperature0" or typ == "Status.temperature1":
+                            self.temp.value = float(item['d']['v'])
+                            self.temp.notify()
+                        if typ == "Status.voltageMeasure":
+                            val = float(item['d']['v'])
+                            if item['d']['p']['c'] == '0':
+                                self.volt0.value = val
+                                self.volt0.notify()
+                            if item['d']['p']['c'] == '1':
+                                self.volt1.value = val
+                                self.volt1.notify()
+                            if item['d']['p']['c'] == '2':
+                                self.volt2.value = val
+                                self.volt2.notify()
+                            if item['d']['p']['c'] == '3':
+                                self.volt3.value = val
+                                self.volt3.notify()
+                            if item['d']['p']['c'] == '4':
+                                self.volt4.value = val
+                                self.volt4.notify()
+                            if item['d']['p']['c'] == '5':
+                                self.volt5.value = val
+                                self.volt5.notify()
+                            if item['d']['p']['c'] == '6':
+                                self.volt6.value = val
+                                self.volt6.notify()
+                            if item['d']['p']['c'] == '7':
+                                self.volt7.value = val
+                                self.volt7.notify()
+
+    @trigger.onproc
+    def do_trigger(self):
         getVoltage= '''{
 	                "i": "%s",
 	                "t": "request",
@@ -73,15 +109,11 @@ class isegSupport(PT.TableBase):
 		                    }
 	                ],
 	                "r": "websocket"
-                    }''' % session_id
-        ws.send(getVoltage)
-        self.calls(ws)
-
-
-        #self.time.value = received
-        #self.time.notify()
-        #self.temp.value = 72.981
-        #self.temp.notify()
+                    }''' % self.session_id
+        self.ws.send(getVoltage)
+        thread = threading.Thread(target=self.thread_call)
+        thread.start()
+        thread.join()
 
 def build():
     return isegSupport('iseg')
