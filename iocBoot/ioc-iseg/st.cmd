33c5ae06cb536c4e1ecaf2ca519aabed077b680f
Ethan Fulcher <fulchere@msu.edu>
2019-08-15 10:29:46 -0400

    cleaned

--- iocBoot/ioc-iseg/st.cmd
+++ iocBoot/ioc-iseg/st.cmd
@@ -4,7 +4,7 @@
 
 epicsEnvSet("PYEPICS_LIBCA", "$(EPICS_BASE)/lib/linux-x86_64/libca.so")
 #epicsEnvSet("DIAGSTD_DISABLE", "YES")
-epicsEnvSet("PREFIX", "ACS_DIAG:IOC_iseg:")
+#epicsEnvSet("PREFIX", "ACS_DIAG:IOC_iseg:")
 
 dbLoadDatabase("${TOP}/dbd/iseg.dbd",0,0)
 iseg_registerRecordDeviceDriver(pdbbase)
@@ -12,7 +12,7 @@ iseg_registerRecordDeviceDriver(pdbbase)
 py "import iseg"
 py "iseg.build()"
 
-dbLoadRecords("$(TOP)/db/iseg.db", "P=$(PREFIX)")
+dbLoadRecords("$(TOP)/db/iseg.db", "P=ACS_DIAG:IOC_iseg:")
 
 iocInit()
 
--- python3.5m/linux-x86_64/iseg.py
+++ python3.5m/linux-x86_64/iseg.py
@@ -1,8 +1,13 @@
 # pyDevSup imports
 import devsup.db as db
 import devsup.ptable as PT
-
 import signal
+
+import json
+import time
+import threading
+from websocket import create_connection
+
 
 # Workaround CTRL+C not working
 signal.signal(signal.SIGINT, signal.SIG_DFL)
@@ -20,11 +25,47 @@ class isegSupport(PT.TableBase):
 
     @trigger.onproc
     def do_trigger(self):
-        self.s.value = self.a.value + self.b.value
-        self.s.notify()
-        self.time.value = "4:23:46"
-        self.temp.value = "72oC"
+        ws = create_connection("ws://192.168.1.101:8080",timeout=40)
+        login_request = '''{
+	                    "i": "",
+	                    "t": "login",
+	                    "c": {
+		                    "l": "user",
+		                    "p": "user",
+		                    "t": ""
+	                    },
+	                    "r": "websocket"
+                        }'''
+        ws.send(login_request)
+        login_message = ws.recv()
+        session_id = json.loads(login_message)['i']
+        getVoltage= '''{
+	                "i": "%s",
+	                "t": "request",
+	                "c": [
+		                    {
+			                    "c": "getItem",
+			                    "p": {
+				                    "p": {
+					                    "l": "0",
+					                    "a": "*",
+					                    "c": "*"
+				                    },
+				                    "i": "Control.voltageSet",
+				                    "v": "",
+				                    "u": ""
+			                    }
+		                    }
+	                ],
+	                "r": "websocket"
+                    }''' % session_id
+        ws.send(getVoltage)
+        received = ws.recv()
+        jsoned = json.loads(received)
+
+        self.time.value = jsoned[0]['c'][1]['d']['v']
         self.time.notify()
+        self.temp.value = 72.981
         self.temp.notify()
 
 def build():
